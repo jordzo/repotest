826c43ba0191e488447d2a9f4c728cadaed9369d
jordzo <6black6venom6@gmail.com>
2018-02-06 08:28:56 +0100

    stos

--- cpp/stos_tab.cpp
+++ cpp/stos_tab.cpp
@@ -12,13 +12,51 @@
 
 using namespace std;
 
+bool czy_pusty(int sp, int sr){
+    if (sp < sr) return true;
+    return false;
+}
+
+void push(int stos[], int &sp, int sr, int dane) {
+    cout << dane << " "; //informacyjny wydruk wstawianej wartości
+    if(czy_pusty(sp, sr)){
+        stos[sp] = dane;
+        sp++;
+    } else {
+        cout << "Błąd przepełnienia stosu!" << endl;
+    }
+}
+
+int pop(int stos[], int &sp) {
+    sp--;
+    return stos[sp];
+}
+
 int main(int argc, char **argv)
 {
-	int *stack; //wskaźnik
+	int *stack; // wskaźnik
     int sr; // rozmiar stosu
+    int sp = 0; // wskaźnik stosu (ang. stack pointer)
     
     cout << "Podaj rozmiar: "; cin >> sr;
-    stack = new
+    stack = new int[sr];
+    
+    srand(time(NULL));
+    for(int i = 0; i < sr+3; i++) {
+        push(stack, sp, sr, rand()%100 + 1);
+    }
+    
+    cout << endl;
+    
+    onp 5 + 7 => 57+
+     5 - 6 + 7 = 56-7+
+    
+    for(int i = 0; i < sr; i++) {
+        cout << pop(stack, sp) << " ";
+    }
+    
 	return 0;
 }
 
+
+TEMAT ONP
