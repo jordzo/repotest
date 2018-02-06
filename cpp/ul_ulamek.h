a723d8c78fe857d21e19b7f88215e4423a4a4c0c
jordzo <6black6venom6@gmail.com>
2018-02-06 08:40:39 +0100

    zmiany

---
+++ cpp/ul_ulamek.h
@@ -0,0 +1,14 @@
+#ifndef UCZEN_H
+#define UCZEN_H
+
+class Ulamek{
+private:
+    int oceny[];
+public:
+    UCZEN();
+    int imie();
+    int nazwisko();
+    int klasa();
+}; //koniec definicji klasy!
+
+#endif
